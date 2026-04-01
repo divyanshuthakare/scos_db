11144e1d407401996f34d7c972ee0d23464b0949
Divyanshu Thakare <divyanshu110599@gmail.com>
2026-04-01 05:06:15 -0700

    Add files via upload

--- src/db/db.js
+++ src/db/db.js
@@ -5,17 +5,9 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-const isProduction = process.env.NODE_ENV === "production";
-
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: isProduction
-    ? { rejectUnauthorized: false }
-    : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 // 🔥 test connection
